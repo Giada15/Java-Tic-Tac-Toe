5bae408e4a2a39f95e8b340aa8bea5eb393adc00
Giada15 <tascheriogiada@gmail.com>
2024-04-25 13:08:57 -0400

    In this commit: -Set up the game grid with a 2D board array and implemented a printBoard function for formatted printing.

--- TicTacToe.java
+++ TicTacToe.java
@@ -4,6 +4,27 @@
 public class TicTacToe {
 
     public static void main(String[] args) {
+
+        char[][] board = {
+            {'_', '_', '_'},
+            {'_', '_', '_'},
+            {'_', '_', '_'},
+        };
+
+        System.out.println("Let's play tic tac toe");
+        printBoard(board);
+
         
+    }
+
+    public static void printBoard(char[][] board){
+        System.out.println("\tnew line");
+        for(int i=0; i<board.length; i++){
+            System.out.print("\t");
+            for(int j=0; j<board[i].length; j++){
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.print("\n\n");
+        }
     }
 }
