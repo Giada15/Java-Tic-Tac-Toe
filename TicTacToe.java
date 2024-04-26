dd0a1323d8d153c0074cf7bbf2afe0ec60d78ae1
Giada15 <tascheriogiada@gmail.com>
2024-04-26 13:33:09 -0400

    In this commit:
    -Created a for loop to iterate 9 times.
    -Implemented an if-else statement to handle user X or user O.
    -Created the askUser function, which returns an int array and receives a 2D array as a parameter. Implemented a while loop within this function to prompt the user to enter a different spot if the spot is taken.
    -Called the askUser function within the if-else statement to assign X or O based on the spot chosen by the user.

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,10 +1,14 @@
+import java.util.Scanner;
+
 /**
  * TicTacToe
  */
 public class TicTacToe {
+    static Scanner scan = new Scanner(System.in);
 
+    
     public static void main(String[] args) {
-
+        
         char[][] board = {
             {'_', '_', '_'},
             {'_', '_', '_'},
@@ -14,11 +18,25 @@ public class TicTacToe {
         System.out.println("Let's play tic tac toe");
         printBoard(board);
 
-        
+        for(int i=0; i<9; i++){
+            if(i%2 == 0){
+                System.out.println("Turn: X");
+                int[] spot = askUser(board);
+                board[spot[0]][spot[1]] = 'X';
+                
+            }else{
+                System.out.println("Turn: O");
+                int[] spot = askUser(board);
+                board[spot[0]][spot[1]] = 'O';
+            }
+
+            printBoard(board);
+        }
+  
     }
 
     public static void printBoard(char[][] board){
-        System.out.println("\tnew line");
+        System.out.println("\t");
         for(int i=0; i<board.length; i++){
             System.out.print("\t");
             for(int j=0; j<board[i].length; j++){
@@ -27,4 +45,19 @@ public class TicTacToe {
             System.out.print("\n\n");
         }
     }
+
+    public static int[] askUser(char[][] board){
+        System.out.print("pick a row and colum number: ");
+        int[] spot = {scan.nextInt(), scan.nextInt()};
+
+        while(board[spot[0]][spot[1]] != '_'){
+            System.out.print("Spot taken, try again: ");
+            spot[0] = scan.nextInt();
+            spot[1] = scan.nextInt();
+        }
+        return spot;
+
+    }
+
+    
 }
